cf5720aecff53d151b634a48c9849eda51989e6d
Jerome Lecomte <jerle76@gmail.com>
2014-03-08 14:35:46 -0500

    Revert "gh-10 game screen: offline mode"
    This reverts commit e2986c73d534045c188a7e5010cf0022bcb5f25c.
    
    Adding a cache manifest triggers the following error on Mobile Safari (iPhone/iPad) when SoundJS register the sound (only in an optimized build aka scripts bundled by concat/requirejs):
       SyntaxError: DOM Exception 12: An invalid or illegal string was specified.
    
    On line 1 of createjs.Sound.handleLoad(), this.request.response passed to this.owner.context.decodeAudioData() is undefined. Moreover, this.request.responseText is [DOM Exception]. It is unclear what the invalid or illegal string is and in what context it appears.

--- Gruntfile.js
+++ Gruntfile.js
@@ -349,31 +349,6 @@ module.exports = function (grunt) {
                 src: '**/*.js'
             }
         },
-        manifest: {
-            dist: {
-                options: {
-                    basePath: 'dist',
-                    cache: [
-                        'http://fonts.googleapis.com/css?family=Press+Start+2P',
-                        'http://themes.googleusercontent.com/static/fonts/pressstart2p/v2/8Lg6LX8-ntOHUQnvQ0E7o2jf3WypfQQP02nP_ZmoBRo.woff'
-                    ],
-                    network: [
-                        'http://www.google.analytics.com/analytics.js'
-                    ],
-                    timestamp: false,
-                    preferOnline: true
-                },
-                src: [
-                    'index.html',
-                    'favicon.ico',
-                    'scripts/*.js',
-                    'images/*.{png,jpg}',
-                    'sounds/*.mp3',
-                    'styles/*.css'
-                ],
-                dest: 'dist/rate-hockey.appcache'
-            }
-        },
         concurrent: {
             server: [
                 'compass',
@@ -434,8 +409,7 @@ module.exports = function (grunt) {
         'requirejs',
         'copy:dist',
         'rev',
-        'usemin',
-        'manifest'
+        'usemin'
     ]);
 
     grunt.registerTask('default', [
